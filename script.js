8dc84776c512b816e4a62d9abe1919634672a50b
Mohit jamle <bhojrajjamle@gmail.com>
2022-06-01 21:52:55 +0530

    commit 1

--- script.js
+++ script.js
@@ -15,18 +15,3 @@ toggleNav.addEventListener("click", function () {
   isShow = !isShow;
   shown();
 });
-
-// .nextButton {
-//     font-size: 30px;
-//     font-weight: 100;
-//     height: fit-content;
-//     padding: 10px 23px;
-//     border-radius: 50%;
-//     border: none;
-//     background-color: white;
-//     box-shadow: 1px 4px 15px 1px #d6d6d6;
-//     cursor: pointer;
-//     position: absolute;
-//     right: 10px;
-//     top: 42%;
-//   }
